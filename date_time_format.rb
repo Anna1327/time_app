ef2a443aad48b6b7df48a767e25d5d35869075a0
Anna1327 <aoi.yatsu@ya.ru>
2023-11-19 21:42:20 +0300

    fix

--- date_time_format.rb
+++ date_time_format.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DateTimeFormat
-  attr_reader :params, :params_available, :valid, :wrong_params
+  attr_reader :params, :valid, :wrong_params
 
   VALID_FORMATS = {
     'year' => '%Y',
@@ -14,33 +14,25 @@ class DateTimeFormat
 
   def initialize(params)
     @params = params.split(',')
-    @available_params = VALID_FORMATS.keys
     @valid = []
     @wrong_params = []
   end
 
   def call
     @params.each do |param|
-      @valid << find_valid_params(param)
-      @wrong_params << find_wrong_params(param)
+      if VALID_FORMATS[param]
+        @valid << VALID_FORMATS[param]
+      else
+        @wrong_params << param
+      end
     end
-    @wrong_params.compact!.to_s
   end
 
   def valid?
-    @params == @valid
-  end
-
-  def find_valid_params(param)
-    param if @available_params.include?(param)
-  end
-
-  def find_wrong_params(param)
-    param unless @available_params.include?(param)
+    @wrong_params.empty?
   end
 
   def prepare_datetime
-    datetime_string = @params.map { |param| VALID_FORMATS[param] }.compact.join('-')
-    Time.now.strftime(datetime_string)
+    Time.now.strftime(@valid.join('-'))
   end
 end
